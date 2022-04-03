ffaa783f6816c35f66919c50ffcc2e78136a2b34
Brenden Niedermeyer <contact@brenden.dev>
2022-04-03 17:08:08 +0200

    initial commit

---
+++ apps/sr-test/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sr-nx-test-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'sr-test';
+}
